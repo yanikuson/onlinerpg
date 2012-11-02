2281fe12e77c57565af4035989cff2a49dcdeeba
Andrew Keturi <andrewketuri@gmail.com>
2012-11-01 22:04:45 -0700

    wrote skeleton for notificationlist.java
    to be implemented next

--- onlinerpg/src/net/alcuria/online/client/NotificationList.java
+++ onlinerpg/src/net/alcuria/online/client/NotificationList.java
@@ -1,5 +1,29 @@
 package net.alcuria.online.client;
 
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+
 public class NotificationList {
 
+	public BitmapFont noticeFont;
+	public int index;
+	public String[] notices;
+	public float[] durations;
+	
+	public NotificationList(){
+		
+	}
+	
+	public void update(){
+		// TODO: update all of the notifications
+		// also check if a notice is invisible and clear it?
+	}
+	
+	public void render(){
+		// TODO: render the list of notices
+	}
+	
+	public void add(String s){
+		// TODO: add string s to the list of notifications
+	}
+	
 }
